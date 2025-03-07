af5f28f18b3b21364d3ba1a0badd72cba9787634
IsraelMancha <mancha.israel.mijares@gmail.com>
2025-03-06 23:57:05 -0600

    vista login terminada

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,5 +1,35 @@
+import React from "react";
+import logo from "../../assets/logo_gc.png";
+import "../../styles/loginStyles.css";
+import "../../styles/globalStyles.css";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container-login">
+      <div className="logo-container">
+        <img src={logo} alt="logotipo de Gruas Coahuila" />
+      </div>
+      <div className="form-container">
+        <form action="">
+          <div className="inputs-container">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Correo Electrónico"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Contraseña"
+            />
+            <button className="login-button">Iniciar Sesión</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/features/ubicaciones/Ubicaciones.jsx
+++ src/features/ubicaciones/Ubicaciones.jsx
@@ -1,5 +1,31 @@
+import LocationCard from "../../components/LocationCard/locationCard";
+import locationIcon from "../../assets/icons/locationIcon";
+import BtnAdd from "../../components/btnAdd";
+import "../../components/btnAdd/btnAdd.css";
+import "./ubicaciones.css";
+
 const Ubicaciones = () => {
-  return <div>ubicaciones</div>;
+  // crear alguna variable o estado que contenga la información de las ubicaciones
+
+  return (
+    <div>
+      <div className="page-title">
+        <h1>Ubicaciones</h1>
+      </div>
+      <div className="btn-add-container">
+        <BtnAdd />
+      </div>
+      <div className="location-cards-container">
+        <LocationCard count={5} icon={locationIcon} location="Piedras Negras" />
+        <LocationCard count={10} icon={locationIcon} location="Nava" />
+        <LocationCard count={3} icon={locationIcon} location="V. Carranza" />
+        <LocationCard count={5} icon={locationIcon} location="Morelos" />
+        <LocationCard count={10} icon={locationIcon} location="Acuña" />
+        <LocationCard count={3} icon={locationIcon} location="Rosita" />
+        <LocationCard count={3} icon={locationIcon} location="Múzquiz" />
+      </div>
+    </div>
+  );
 };
 
 export default Ubicaciones;
